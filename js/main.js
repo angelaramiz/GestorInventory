840c5f54657c9104a2a562b53cc3a43166c3795f
angelaramiz <angelaramiz22@gmail.com>
2024-10-13 00:26:14 -0600

    up inports

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Importaciones
-import { db, dbInventario ,inicializarDB, inicializarDBInventario, cargarCSV, descargarCSV, cargarDatosEnTabla, cargarDatosInventarioEnTablaPlantilla, resetearBaseDeDatos, generarPlantillaInventario } from './db-operations.js';
+import { db, dbInventario ,inicializarDB, inicializarDBInventario, cargarCSV, descargarCSV, cargarDatosEnTabla, cargarDatosInventarioEnTablaPlantilla, resetearBaseDeDatos, generarPlantillaInventario, descargarInventarioPDF, descargarInventarioCSV } from './db-operations.js';
 import { mostrarMensaje } from './logs.js';
 import { agregarProducto, buscarProducto, buscarProductoParaEditar, guardarCambios, eliminarProducto, buscarProductoInventario, guardarInventario } from './product-operations.js';
 import { toggleEscaner } from './scanner.js';
