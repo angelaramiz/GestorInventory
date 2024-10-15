358baf64b7ef2214f7e5bafe3afa4914551542de
angelaramiz <144601522+angelaramiz@users.noreply.github.com>
2024-10-15 16:29:52 -0600

    Up4.2

--- js/db-operations.js
+++ js/db-operations.js
@@ -264,7 +264,7 @@ export function descargarInventarioPDF() {
             );
             doc.text(`Fecha de Caducidad: ${item.fechaCaducidad}`, 10, yPos + 15);
             doc.text(`Comentarios: ${item.comentarios}`, 10, yPos + 20);
-            yPos += 25
+            yPos += 30;
             if (yPos > 280) {
                 doc.addPage();
                 yPos = 25;
